2750b21a4d6b6d82c72f068d2875c1efb28fed5f
Grimmbol <krogstie.oystein@gmail.com>
2021-04-25 16:38:17 +0200

    add some basic event handling and state

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+let displayValue = 1;
+
+function handleClick(event) {
+    if(displayValue > 10) {
+	displayValue = 0;
+    }
+    displayValue = displayValue + 1;
+    updateDisplay(displayValue);
+}
+
+function updateDisplay(newValue) {
+    let displayHandle = document.querySelector(".display");
+    displayHandle.textContent = newValue;
+}
+
 function init(event) {
     // Finn noden som har klassen textbox og lagre referansen i en variabel
     let textBoxHandle = document.querySelector(".textbox");
 
     // Sett tekstinnholdet i noden lik en ny bit med tekst
     textBoxHandle.textContent = "Javascript lasta og klar!";
+    
+    updateDisplay(displayValue);
+    let enterKeyHandle = document.querySelector(".enter");
+    enterKeyHandle.addEventListener('click', handleClick);
 }
 
 window.addEventListener('load', init);
